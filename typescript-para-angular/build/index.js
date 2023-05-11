698b8db1d00ee4bfb1b3d19d008c8c27dbf571ec
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-05-11 18:08:57 -0300

    Tipagens em TypeSript

--- typescript-para-angular/build/index.js
+++ typescript-para-angular/build/index.js
@@ -7,3 +7,38 @@ printaObjetos({
     vulgo: "Batman",
 });
 console.log(`Olá mundo!`);
+// tipos primitivos: boolean, number, string.
+let ligado = true;
+let nome = 'Vinícius';
+let idade = 23;
+let altura = 1.9;
+// Tipos Especiais
+// null
+// undefined
+let nulo = null;
+let indefinido = undefined;
+//Tipod Abrangentes: any, void
+function executaQuery() { }
+let retornoView = false;
+//objeto
+// Objeto sem previsibilidade
+let produto = {
+    name: 'ViníAlias',
+    cidade: 'Osasco'
+};
+let meuProduto = {
+    nome: "tênis",
+    preco: 89.99,
+    unidades: 9,
+};
+// Array 
+let dados = ['Vinicius', 'Eva', 'Adão'];
+let dados2 = ['Vinicius', 'Eva', 'Adão'];
+let infos = ['Vinicius', 22, "xccxcx"];
+// Tuplas
+let boleto = ['Agua conta', 19.09, 3384342343];
+/// Array Método
+// Mesmos metodos para manipular do JS
+// Datas
+let birthday = new Date("22-06-2023 18:00");
+console.log(birthday.toString());
--- typescript-para-angular/src/index.ts
+++ typescript-para-angular/src/index.ts
@@ -13,3 +13,64 @@ printaObjetos({
 });
 
 console.log(`Olá mundo!`);
+
+
+// tipos primitivos: boolean, number, string.
+let ligado:boolean = true;
+let nome: string = 'Vinícius';
+let idade: number = 23;
+let altura: number = 1.9;
+
+// Tipos Especiais
+// null
+// undefined
+
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+//Tipod Abrangentes: any, void
+
+function executaQuery(): void{}
+let retornoView: any = false;
+
+//objeto
+
+// Objeto sem previsibilidade
+let produto: object = {
+    name: 'ViníAlias',
+    cidade: 'Osasco'
+};
+
+//Objeto Tipado - Com previsibilidade
+type ProdutoLoja = {
+    nome:string,
+    preco: number,
+    unidades: number;
+}
+
+let meuProduto: ProdutoLoja = {
+    nome: "tênis",
+    preco: 89.99,
+    unidades: 9,
+}
+
+// Array 
+
+let dados: string[] =['Vinicius', 'Eva', 'Adão'];
+let dados2: Array<string> = ['Vinicius', 'Eva', 'Adão'];
+
+let infos: (string | number)[] = ['Vinicius', 22, "xccxcx"];
+
+// Tuplas
+
+let boleto: [string, number, number] = ['Agua conta', 19.09, 3384342343]
+
+
+/// Array Método
+
+// Mesmos metodos para manipular do JS
+
+// Datas
+
+let birthday : Date = new Date("2023-06-22 18:00");
+console.log(birthday.toString());
